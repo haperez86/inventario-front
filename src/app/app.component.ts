1dee1485017fbef18f3727fcd22731a2917edb46
Henry Perez <haperez86@gmail.com>
2025-05-25 18:30:41 -0500

    se realiza menu para Mercancia y Usuarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,9 @@ import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
 import { FormularioMercanciaComponent } from './components/formulario-mercancia/formulario-mercancia.component';
 import { ListaMercanciaComponent } from './components/lista-mercancia/lista-mercancia.component';
-import { FiltroBusquedaComponent } from "./components/filtro-busqueda/filtro-busqueda.component";
-import { PruebasComponent } from "./components/pruebas/pruebas.component";
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { MenuItem } from 'primeng/api';
+
 
 
 @Component({
@@ -17,14 +18,29 @@ import { PruebasComponent } from "./components/pruebas/pruebas.component";
     SidebarModule,
     ButtonModule,
     RouterOutlet,
+    PanelMenuModule,
     FormularioMercanciaComponent,
-    ListaMercanciaComponent,
-    FiltroBusquedaComponent,
-    PruebasComponent
+    ListaMercanciaComponent
 ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'inventario-front';
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Mercancía',
+        icon: 'pi pi-box',
+        routerLink: '/mercancia'
+      },
+      {
+        label: 'Usuarios',
+        icon: 'pi pi-users',
+        routerLink: '/usuarios'
+      }
+    ];
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ListaMercanciaComponent } from './components/lista-mercancia/lista-mercancia.component';
+import { UsuarioSelectorComponent } from './components/usuario-selector/usuario-selector.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: 'mercancia', component: ListaMercanciaComponent },
+  { path: 'usuarios', component: UsuarioSelectorComponent },
+  { path: '', redirectTo: '/mercancia', pathMatch: 'full' },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
